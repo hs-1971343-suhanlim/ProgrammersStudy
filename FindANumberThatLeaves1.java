547e89361a426d42d80fc25ccc289c3cd4cb2e80
suhanlim <soohan2009@naver.com>
2023-03-06 00:09:09 +0900

    나머지가 1이 되는 수 찾기 [java] level 1

---
+++ FindANumberThatLeaves1.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        while(true){
+            if(n%++answer==1)
+                break;
+        }
+        return answer;
+    }
+}
+public class FindANumberThatLeaves1 {
+}
